38f5740b8e3e1e2ef2dfe95f0f2a57f2eb18d31f
kehang <815432837@qq.com>
2020-01-17 08:50:34 +0800

    资产和变更类型添加

--- src/modules/change/status.js
+++ src/modules/change/status.js
@@ -6,7 +6,7 @@ const changeStatus = {
         {status:3,name:'理财',icon:''},
         {status:4,name:'红包',icon:''},
         {status:5,name:'租金',icon:''},
-        {status:100,name:'其他',icon:''},
+        {status:100,name:'其他',icon:''}
     ],
     give:[
         //status 101-300 支出
@@ -18,22 +18,22 @@ const changeStatus = {
         {status:106,name:'买菜',icon:''},
         {status:107,name:'娱乐',icon:''},
         {status:108,name:'租金',icon:''},
-        {status:105,name:'房贷',icon:''},
-        {status:105,name:'服装',icon:''},
-        {status:105,name:'礼物',icon:''},
-        {status:105,name:'旅行',icon:''},
-        {status:105,name:'数码',icon:''},
-        {status:105,name:'人情',icon:''},
-        {status:105,name:'社交',icon:''},
-        {status:105,name:'长辈',icon:''},
-        {status:105,name:'医疗',icon:''},
-        {status:105,name:'学习',icon:''},
-        {status:105,name:'通讯',icon:''},
-        {status:105,name:'健身',icon:''},
-        {status:105,name:'理发',icon:''},
-        {status:105,name:'约会',icon:''},
-        {status:105,name:'礼物',icon:''},
-        {status:300,name:'其他',icon:''}
+        {status:109,name:'房贷',icon:''},
+        {status:110,name:'服装',icon:''},
+        {status:111,name:'礼物',icon:''},
+        {status:112,name:'旅行',icon:''},
+        {status:113,name:'数码',icon:''},
+        {status:114,name:'人情',icon:''},
+        {status:115,name:'社交',icon:''},
+        {status:116,name:'长辈',icon:''},
+        {status:117,name:'医疗',icon:''},
+        {status:118,name:'学习',icon:''},
+        {status:119,name:'通讯',icon:''},
+        {status:120,name:'健身',icon:''},
+        {status:121,name:'理发',icon:''},
+        {status:122,name:'约会',icon:''},
+        {status:123,name:'礼物',icon:''},
+        {status:300,name:'其他',icon:''},
     ]
 };
 export default changeStatus
--- src/modules/property/status.js
+++ src/modules/property/status.js
@@ -1,4 +1,34 @@
-const creditCard = Math.pow(2,10);
 export default {
-    [creditCard]:'信用卡'
+    debt:[
+        //status 1-100 欠债
+        {status:1,name:'负债',icon:''},
+        {status:2,name:'花呗',icon:''},
+        {status:3,name:'信用卡',icon:''},
+        {status:4,name:'京东白条',icon:''},
+        {status:5,name:'借呗',icon:''},
+        {status:6,name:'苏宁任性付',icon:''},
+        {status:100,name:'其他',icon:''}
+    ],
+    save:[
+        //status 101-300 存款
+        {status:101,name:'现金',icon:''},
+        {status:102,name:'支付宝',icon:''},
+        {status:103,name:'微信',icon:''},
+        {status:104,name:'基金',icon:''},
+        {status:105,name:'股票',icon:''},
+        {status:106,name:'理财产品',icon:''},
+        {status:107,name:'借出',icon:''},
+        {status:108,name:'工商银行',icon:''},
+        {status:109,name:'招商银行',icon:''},
+        {status:110,name:'农业银行',icon:''},
+        {status:111,name:'中国银行',icon:''},
+        {status:112,name:'建设银行',icon:''},
+        {status:113,name:'交通银行',icon:''},
+        {status:114,name:'中信银行',icon:''},
+        {status:115,name:'浦发银行',icon:''},
+        {status:116,name:'广发银行',icon:''},
+        {status:117,name:'光大银行',icon:''},
+        {status:118,name:'兴业银行',icon:''},
+        {status:300,name:'其他',icon:''}
+    ]
 }
